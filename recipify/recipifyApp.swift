2c6f575801da189176941d0411f4e260d14ae77a
timo2k <timo.ischen@gmail.com>
2024-10-15 13:19:22 +0200

    Initial Commit

---
+++ recipify/recipifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipifyApp.swift
+//  recipify
+//
+//  Created by Timo Ischen on 15.10.24.
+//
+
+import SwiftUI
+
+@main
+struct recipifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipifyTests/recipifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipifyTests.swift
+//  recipifyTests
+//
+//  Created by Timo Ischen on 15.10.24.
+//
+
+import Testing
+@testable import recipify
+
+struct recipifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
